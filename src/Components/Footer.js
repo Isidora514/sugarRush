c76800e0e73121d17deb66a012f9ed2a69722bd1
George Almonte <41764662+georgeaf18@users.noreply.github.com>
2021-06-12 13:52:53 -0400

    lots of styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,38 +1,42 @@
 import React from "react";
+import bigChicken from "../images/big-chicken-pplaceholder.png"
 
 
 export default function Characters(props) {
 
   const content = [
-    "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-     "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+     "it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
   ]
 
   return (
     <section className="footerSection">
 
-<div className="firstFooter">
-      <td bgcolor="#56d0fa"> 
-      <h1 color="#ffff">Why sugar rush?</h1>
-      <p>{content[0]}</p>
-      </td>
+    <div className="firstFooter">
+      <div className="blue-bg px-5 pb-5"> 
+        <h1 className="text-white pb-3 ">WHY SUGAR RUSH?</h1>
+        <p className="text-left">{content[0]}</p>
+      </div>
     </div>
-    <div className="midImage">
-    <img alt="bigchicken" />
-    </div>
+
+    <img className="midImage" src={bigChicken} alt="bigchicken" />
 
     <div className="secondFooter">
-      <td bgcolor="#eb4e72"> 
-      <h1 color="#ffff">Article: History with my son</h1>
+      <div className="magenta-bg px-5 pb-2"> 
+      <h1 color="#ffff">ARTICLE: <br /> HISTORY WITH MY SON</h1>
       <p>{content[1]}</p>
-      </td>
+      </div>
     </div>
 
-    <div className="thirdFooter">
-      <td> 
-      <h1 color="#eb4e72">Bye Bye see you soon!</h1>
-      <p>{content[1]}</p>
-      </td>
+    <div className="thirdFooter mt-3">
+      <h1 className="magenta mb-4">BYE BYE SEE YOU SOON!</h1>
+      <p className="disclaimer">For Entertainment, Educational and Music purpose, only for kids</p>
+    </div>
+
+    <div>
+      <small>
+        Privacy Policy &#124; Terms of use &#124; Surgarrushjams.org
+      </small>
     </div>
 
     </section>
