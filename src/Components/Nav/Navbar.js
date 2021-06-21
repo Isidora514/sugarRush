cfdaa28f6346c137623fc5558726a13cd16cc8dc
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-06-21 11:28:52 -0400

    routing

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -14,10 +14,10 @@ export default function Navbar(props) {
         <div id="logoDiv">
         <img id="navLogo" src={logo} alt="logo" />
         </div>
-        <li><a href="/"><h1>HOME</h1></a></li>
-        <li><a href="/about"><h1>ABOUT</h1></a></li>
-        <li><a href="/media"><h1>MEDIA</h1></a></li>
-        <li><a href="/contact"><h1>CONTACT</h1></a></li>
+        <li><a href="https://sugar-rush-demo.netlify.app/"><h1>HOME</h1></a></li>
+        <li><a href="https://sugar-rush-demo.netlify.app/about"><h1>ABOUT</h1></a></li>
+        <li><a href="https://sugar-rush-demo.netlify.app/media"><h1>MEDIA</h1></a></li>
+        <li><a href="https://sugar-rush-demo.netlify.app/contact"><h1>CONTACT</h1></a></li>
       </div>
     </nav>
   ) 
